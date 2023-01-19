9501b2e94d357073134531eb0400978d0087aad2
Dieter <dburgstaller@gmx.de>
2023-01-19 04:29:35 +0100

    Master Laravel9, Vue3 & Inertia Fullstack 2023 Working with the Database Part 1
    Lesson 35- Lesson 46

---
+++ resources/js/Pages/Listing/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-for="listing in listings " :key="listing.id">
+    <Link :href="`/listing/${listing.id}`" >
+      <Listingaddress :listing="listing" />
+     </Link>
+  </div>
+
+</template>
+<script setup>
+  import {Link} from '@inertiajs/inertia-vue3';
+  import Listingaddress from '../../Components/ListingAddress.vue'
+   defineProps({
+    listings: Array,
+  })
+</script>
+<script>
+ import MainLayout from '../../Layouts/MainLayout.vue'
+export default {
+  layout: MainLayout
+}
+</script>
