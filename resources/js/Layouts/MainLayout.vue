07d2b5899ea9cdc04acb24ec14d00c94a684bf81
Dieter <dburgstaller@gmx.de>
2023-01-20 03:31:58 +0100

    Master Laravel 9, Vue3 &Inertia Fullstack 2023 Data Validation

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,13 +1,38 @@
 <template>
   <Link href="/">Main Page</Link>&nbsp;
-  <Link href="/hello">Show Page</Link>
+  <Link href="/hello">Show Page</Link>&nbsp;
+<Link href="/listing/create">Create</Link>
+  <div v-if="flashSuccess" class="success">
+    {{ flashSuccess }}
+  </div>
+  <!-- <div>
+    {{ y }}
+  </div> -->
+
   <!-- <div>The page with time {{timer}}</div> -->
   <slot>Default</slot>
 </template>
 <script setup>
-  import {Link} from'@inertiajs/inertia-vue3';
-  import {ref} from 'vue';
+  import {computed} from 'vue';
+  import {Link, usePage } from'@inertiajs/inertia-vue3';
+ // import {ref} from 'vue';
 
+  // const x = ref(0);
+  // const y = computed(() => x.value * 2)
+
+  //page.props.value.flash.success
+ const page = usePage()
+ const flashSuccess = computed(
+  ()=>page.props.value.flash.success,
+  )
   // const timer = ref(0);
   // setInterval(() => timer.value++, 1000)
 </script>
+
+<style scoped>
+  .success {
+    background-color: green;
+    color: white;
+
+  }
+</style>
